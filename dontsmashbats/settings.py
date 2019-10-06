f681d3256ebc649a8dacf19e77d87ccd41ee2966
Sam Oehlert <sam.oehlert@gmail.com>
2019-10-06 14:21:58 -0500

    fix(static): fix paths for static and media

--- dontsmashbats/settings.py
+++ dontsmashbats/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bats.apps.BatsConfig',
-    'storages',
 ]
 
 
@@ -126,6 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
